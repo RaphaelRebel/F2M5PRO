85cf3b28181b7ebb7311e6111efd046ad38e1ce4
MaxBouricius <55273830+MaxBouricius@users.noreply.github.com>
2021-10-29 14:51:50 +0200

    changes

--- f2m5-social-responsibility/_private/views/homepage.php
+++ f2m5-social-responsibility/_private/views/homepage.php
@@ -1,10 +1,9 @@
 <?php $this->layout('layouts::website');?>
 <link rel="stylesheet" href="<?php echo site_url( '/css/home.css' ) ?>" media="all">
-
+<div>
+        
+</div>
 <div class="top">
-        <div class="nav">
-            <img src="" alt="">    
-        </div>
             <div class="eyeCatcher">
                     <h2>Denk jij ook wel eens een van deze dingen?</h2>
                     <div class="eyes">
